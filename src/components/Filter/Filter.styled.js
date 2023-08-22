be82c734d72485175a4f372d99bd138d8405d7bd
Dmytro Medvediev <dsisitkin@gmail.com>
2023-08-22 14:11:08 +0200

    Update Filter.styled.js

--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -1,18 +1,5 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.label`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 10px;
-  width: 100%;
-`;
-
-export const Label = styled.p`
-  font-weight: bold;
-  margin-bottom: 5px;
-  width: 100%;
-`;
-
 export const Input = styled.input`
   padding: 5px;
   margin-top: 5px;
